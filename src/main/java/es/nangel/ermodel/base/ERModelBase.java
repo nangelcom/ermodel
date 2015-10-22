1950bfc98fac43a003c2b4a2751302322828ff54
Ángel Luis López Monterroso <angel9484@gmail.com>
2015-10-22 21:05:23 +0200

    Adding modularity for parsers.

--- src/main/java/es/nangel/ermodel/base/ERModelBase.java
+++ src/main/java/es/nangel/ermodel/base/ERModelBase.java
@@ -2,29 +2,44 @@ package es.nangel.ermodel.base;
 
 import es.nangel.ermodel.model.Entity;
 
+import javax.swing.text.html.parser.Parser;
+
 /**
  * Created by nangel on 21/10/15.
  */
 public final class ERModelBase {
     private static final String FIRST_TOKEN_GENERATE = "generate";
 
-    public static void main(final String args[]) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static void main(final String args[]) throws Exception {
         if (args.length == 3) {
             if (FIRST_TOKEN_GENERATE.equals(args[0])) {
                 String[] plugins = args[1].split(",");
                 for (String plugin : plugins) {
-                    Class<?> entityEntryPoint;
-                    if (plugin.contains(".")) {
-                        entityEntryPoint = Class.forName(plugin);
-                    } else {
-                        entityEntryPoint = Class.forName("es.nangel.ermodel." + plugin + "." + plugin);
-                    }
-                    Entity entityCreator = (Entity) entityEntryPoint.newInstance();
+                    Entity entityCreator = getInstanceWithNamespace(plugin, Entity.class);
                     for (String file : args[2].split(",")) {
+                        String[] engine = file.split("\\.");
+                        Parser parser = getInstanceWithNamespace(engine[engine.length - 1], Parser.class);
+                        parser.parse(file);
                         entityCreator.process(file);
                     }
                 }
             }
         }
+    }
+
+    private static <T> T getInstanceWithNamespace(String className, Class<T> tClass) throws Exception {
+        String classNamePath = className;
+        if (!className.contains(".")) {
+            classNamePath = "es.nangel.ermodel." + className + "." + classNamePath;
+        }
+        return getInstanceForClass(classNamePath, tClass);
+    }
+
+    private static <T> T getInstanceForClass(String className, Class<T> tClass) throws Exception {
+        return (T) loadClass(className).newInstance();
+    }
+
+    private static Class<?> loadClass(String className) throws Exception {
+        return Class.forName(className);
     }
 }
